035bbee684eeea1e57326bd6cf3bad7b926637dc
Ali Rizvi <frontear4594@gmail.com>
2019-11-13 16:50:28 -0500

    Use core profile rendering
    This is the most supported and recommended way of drawing opengl stuff, but as you can see, there is quite the boilerplate excess. Theikos will abstract this nonsense away.

--- src/main.cpp
+++ src/main.cpp
@@ -5,31 +5,79 @@ int main() {
     auto display = theikos::Display("Theikos", 640, 480);
     std::cout << display;
 
-    /*
-    // -- IMMEDIATE MODE
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    glOrtho(0, display.getWidth(), display.getHeight(), 0, 1, -1);
-    glMatrixMode(GL_MODELVIEW);
-    // -- IMMEDIATE MODE
-    */
+    // -- RETAINED MODE
+    GLuint array;
+    glGenVertexArrays(1, &array);
+    glBindVertexArray(array);
 
-    auto width = 400, height = 200, x = (display.getWidth() - width) / 2, y = (display.getHeight() - height) / 2;
+    const GLfloat vert[] = {
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.0f, 0.5f, 0.0f
+    };
+
+    GLuint buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vert), vert, GL_STATIC_DRAW);
+
+    const char *v_shader =
+        R"glsl(
+        #version 330 core
+
+        layout(location = 0) in vec3 coord;
+
+        void main() {
+            gl_Position.xyz = coord;
+            gl_Position.w = 1.0;
+        }
+        )glsl";
+
+    const char *f_shader =
+        R"glsl(
+        #version 330 core
+
+        out vec3 color;
+
+        void main() {
+            color = vec3(0.5, 0.5, 1.0);
+        }
+        )glsl";
+
+    GLuint s_vertex = glCreateShader(GL_VERTEX_SHADER);
+    GLuint s_fragment = glCreateShader(GL_FRAGMENT_SHADER);
+
+    glShaderSource(s_vertex, 1, &v_shader, nullptr);
+    glCompileShader(s_vertex); // assumed success
+
+    glShaderSource(s_fragment, 1, &f_shader, nullptr);
+    glCompileShader(s_fragment); // assumed success
+
+    GLuint program = glCreateProgram();
+    glAttachShader(program, s_vertex);
+    glAttachShader(program, s_fragment);
+    glLinkProgram(program); // assumed success
+
+    glDetachShader(program, s_vertex);
+    glDetachShader(program, s_fragment);
+
+    glDeleteShader(s_vertex);
+    glDeleteShader(s_fragment);
+    // -- RETAINED MODE
+
     do {
         glClear(GL_COLOR_BUFFER_BIT);
-        /*
-        // -- IMMEDIATE MODE
-        glColor3f(0.5f, 0.5f, 1.0f);
-        glBegin(GL_QUADS);
-        {
-            glVertex2f(x, y);
-            glVertex2f(x + width, y);
-            glVertex2f(x + width, y + height);
-            glVertex2f(x, y + height);
-        }
-        glEnd();
-        // -- IMMEDIATE MODE
-        */
+
+        // -- RETAINED MODE
+        glUseProgram(program);
+
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, buffer);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDisableVertexAttribArray(0);
+        // -- RETAINED MODE
 
         display.tick();
     }
